e420fc52be71bad2806e1cbf1853f91ba607a0d9
b0ch3n <mbochen1@gmail.com>
2015-05-21 20:46:26 +0200

    ll

--- client/qtdesigner.py
+++ client/qtdesigner.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'qtdesigner.ui'
 #
-# Created: Mon May 18 20:28:14 2015
+# Created: Thu May 21 20:31:33 2015
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,7 +26,7 @@ except AttributeError:
 class Ui_MplMainWindow(object):
     def setupUi(self, MplMainWindow):
         MplMainWindow.setObjectName(_fromUtf8("MplMainWindow"))
-        MplMainWindow.resize(448, 260)
+        MplMainWindow.resize(402, 260)
         self.mplcentralwidget = QtGui.QWidget(MplMainWindow)
         self.mplcentralwidget.setObjectName(_fromUtf8("mplcentralwidget"))
         self.verticalLayout = QtGui.QVBoxLayout(self.mplcentralwidget)
@@ -50,9 +50,19 @@ class Ui_MplMainWindow(object):
         self.verticalLayout.addWidget(self.mpl)
         MplMainWindow.setCentralWidget(self.mplcentralwidget)
         self.menubar = QtGui.QMenuBar(MplMainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 448, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 402, 21))
         self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menu_File = QtGui.QMenu(self.menubar)
+        self.menu_File.setObjectName(_fromUtf8("menu_File"))
         MplMainWindow.setMenuBar(self.menubar)
+        self.mplactionOpen = QtGui.QAction(MplMainWindow)
+        self.mplactionOpen.setObjectName(_fromUtf8("mplactionOpen"))
+        self.mplactionQuit = QtGui.QAction(MplMainWindow)
+        self.mplactionQuit.setObjectName(_fromUtf8("mplactionQuit"))
+        self.menu_File.addAction(self.mplactionOpen)
+        self.menu_File.addSeparator()
+        self.menu_File.addAction(self.mplactionQuit)
+        self.menubar.addAction(self.menu_File.menuAction())
 
         self.retranslateUi(MplMainWindow)
         QtCore.QMetaObject.connectSlotsByName(MplMainWindow)
@@ -60,5 +70,8 @@ class Ui_MplMainWindow(object):
     def retranslateUi(self, MplMainWindow):
         MplMainWindow.setWindowTitle(_translate("MplMainWindow", "MainWindow", None))
         self.mplpushButton.setText(_translate("MplMainWindow", "PushButton", None))
+        self.menu_File.setTitle(_translate("MplMainWindow", "&File", None))
+        self.mplactionOpen.setText(_translate("MplMainWindow", "Open", None))
+        self.mplactionQuit.setText(_translate("MplMainWindow", "Quit", None))
 
 from mplwidget import MplWidget
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 __author__ = 'Mateusz'
-
+'''
 from socket import *
 s = socket(AF_INET, SOCK_STREAM) #utworzenie gniazda
 s.connect(('localhost', 8888)) # nawiazanie polaczenia
 tm = s.recv(1024) #odbior danych (max 1024 bajtów)
 s.close()
 print(tm)
+'''
+
+
+from __future__ import  with_statement
+import numpy as np
+import sys
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+from client.qtdesigner import Ui_MplMainWindow
+
+class DesignerMainWindow(QtGui.QMainWindow, Ui_MplMainWindow):
+    def __init__(self, parent=None):
+        super(DesignerMainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        QtCore.QObject.connect(self.mplpushButton,)
+
+
+
